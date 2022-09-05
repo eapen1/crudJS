bd2dcbd0f83cf9cb7ad24cf90f75fde930214473
Elfego Adonias Apen Jon <eaapen@dominiodecamd.local>
2022-09-05 15:19:15 -0600

    updating

--- header.php
+++ header.php
@@ -10,7 +10,8 @@
     <title>CRUD JS</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-light">
+<div class="container">
+  <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
---
+++ personas.php
@@ -0,0 +1,8 @@
+<?php 
+require_once("header.php");
+?>
+<form action="">
+    <label for="nombre">Nombre</label>
+</form>
+<?php
+require_once("footer.php");
