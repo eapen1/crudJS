97171c74fde833237484807f043ff977dc815311
Elfego Adonias Apen Jon <eaapen@dominiodecamd.local>
2022-09-05 15:33:40 -0600

    update

--- personas.php
+++ personas.php
@@ -1,8 +1,15 @@
 <?php 
 require_once("header.php");
 ?>
-<form action="">
-    <label for="nombre">Nombre</label>
-</form>
+<div class="col-9">
+    <br>
+    <br>
+    <form class="" action="">
+        <label for="nombre">Nombre:</label>
+        <input type="text" required>
+        <label for="apellidos">Apellidos:</label>
+        <input type="text" required>
+    </form>
+</div>
 <?php
 require_once("footer.php");
